b2dba99d54a6d4b3b24d8b84f48e943c3569e140
Tein Van Der Lugt <wolf6333@arc-login01.arcus-htc.arc.local>
2022-02-17 17:46:48 +0000

    New ARC output - job7 almost finished?

---
+++ panda-files/arc-output/job7/job7.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --partition=long
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=16G
+#SBATCH --time=120:00:00
+
+time $DATA/panda-data/panda -c -i safe nsco1_facets.pi --known-vertices=known_vertices_from_job4_reordered
