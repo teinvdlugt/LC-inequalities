45592514739d308df430cd269edd0db4f993b7de
Tein Van Der Lugt <wolf6333@arc-login01.arcus-htc.arc.local>
2022-02-17 12:49:06 +0000

    another arc output

--- panda-files/arc-output/job6/job6.sh
+++ panda-files/arc-output/job6/job6.sh
@@ -2,4 +2,4 @@
 #SBATCH --partition=devel
 #SBATCH --ntasks-per-node=1
 
-time $DATA/panda-data/panda -c -i safe nsco1_facets.pi --known-vertices=known_vertices_from_job4
+time $DATA/panda-data/panda -c -i safe nsco1_facets.pi --known-vertices=known_vertices_from_job4_reordered
